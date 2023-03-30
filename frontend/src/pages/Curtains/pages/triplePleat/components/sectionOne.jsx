a85a060c0e42b75aac95448fd0ba951f8fcbe849
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-03-30 15:35:10 +0530

    Somthing is added

--- frontend/src/pages/Curtains/pages/triplePleat/components/sectionOne.jsx
+++ frontend/src/pages/Curtains/pages/triplePleat/components/sectionOne.jsx
@@ -13,12 +13,12 @@ function SectionOne() {
             <p>Introducing Thread Curtains UK's exquisite Triple Pleat Curtains, an elegant addition to any home seeking a sophisticated touch of luxury. Crafted with the finest materials and available in a wide range of colors, these curtains not only add a splash of style to your living space, but also provide the perfect balance of privacy and light filtration.</p>
 
             <div className="space-y-2">
-              <h2>Quality Materials and Craftsmanship:</h2>
+              <h2 className='text-lg font-medium'>Quality Materials and Craftsmanship:</h2>
               <p>Thread Curtains UK's Triple Pleat Curtains are made from premium-quality, heavyweight fabric that is both durable and soft to the touch. These curtains feature a high thread count, ensuring that they maintain their stunning appearance for years to come. Meticulously sewn by skilled artisans, each curtain exhibits exceptional craftsmanship, with strong seams and flawless triple pleats that are designed to withstand the test of time.</p>
             </div>
 
             <div className="space-y-2">
-              <h2>Elegant Triple Pleat Design:</h2>
+              <h2 className='text-lg font-medium'>Elegant Triple Pleat Design:</h2>
               <p>The triple pleat design is a sophisticated and stylish curtain style. It features three pleats close together at the top, creating an elegant and polished appearance. The pleats are evenly spaced and held together by a strong, adjustable cord, allowing you to customize the fullness and fit of the curtains to suit your windows perfectly.</p>
             </div>
 
@@ -34,32 +34,32 @@ function SectionOne() {
 
 
       <div className="space-y-2">
-              <h2>Wide Range of Colours:</h2>
+              <h2 className='text-lg font-medium'>Wide Range of Colours:</h2>
               <p>Thread Curtains UK's Triple Pleat Curtains come in an extensive array of colours, from soft neutrals to bold, statement-making hues. This ensures that you can find the perfect shade to complement your existing décor and colour scheme. Whether you prefer a minimalist look with subtle tones or a more dramatic effect with vibrant colours, these curtains are sure to enhance the aesthetic appeal of your space.</p>
             </div>
 
         <div className="space-y-2">
-          <h2>Excellent Light Filtration and Privacy:</h2>
+          <h2 className='text-lg font-medium'>Excellent Light Filtration and Privacy:</h2>
           <p>Crafted with a heavy-weight fabric, our Triple Pleat Curtains provide optimal light filtration, gently diffusing sunlight and creating a warm, inviting atmosphere in your room. The curtains also provide exceptional privacy, ensuring that you can enjoy your home without worrying about prying eyes.</p>
         </div>
 
         <div className="space-y-2">
-          <h2>Easy Installation and Maintenance:</h2>
+          <h2 className='text-lg font-medium'>Easy Installation and Maintenance:</h2>
           <p>Thread Curtains UK's Triple Pleat Curtains are designed for hassle-free installation. They come with sturdy, easy-to-use hooks that can be attached directly to your curtain pole or track, allowing you to hang your curtains with minimal effort. Additionally, the curtains are machine-washable and easy to care for, ensuring that they remain fresh and look their best for years to come.</p>
         </div>
 
         <div className="space-y-2">
-          <h2>Suitable for a Variety of Spaces:</h2>
+          <h2 className='text-lg font-medium'>Suitable for a Variety of Spaces:</h2>
           <p>Our Triple Pleat Curtains are incredibly versatile and can be used in various rooms throughout your home. They are perfect for living rooms, dining rooms, bedrooms, and even home offices. Their classic design and luxurious feel make them an excellent choice for both traditional and contemporary interiors.</p>
         </div>
 
         <div className="space-y-2">
-          <h2>Eco-Friendly and Sustainable:</h2>
+          <h2 className='text-lg font-medium'>Eco-Friendly and Sustainable:</h2>
           <p>At Thread Curtains UK, we care about the environment and strive to create products that are eco-friendly and sustainable. Our Triple Pleat Curtains are made from responsibly sourced materials, ensuring minimal impact on the environment. By choosing our curtains, you are making a conscious decision to support sustainable and eco-friendly practices.</p>
         </div>
 
         <div className="space-y-2">
-          <h2>Customer Satisfaction Guaranteed:</h2>
+          <h2 className='text-lg font-medium'>Customer Satisfaction Guaranteed:</h2>
           <p>We take pride in the quality of our products and are confident that you will love your new Triple Pleat Curtains. If you are not completely satisfied with your purchase, please contact our dedicated customer service team, who will be more than happy to assist you with any concerns or issues.</p>
         </div>
 
