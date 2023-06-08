b4c0de00b09f05a1c47526799369fe8232727476
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-06-08 17:44:46 +0530

    Somthing is added

--- backend/routes/pay.js
+++ backend/routes/pay.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router()
-const stripe = require('stripe')('API KEYS')
+const stripe = require('stripe')('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')
 
 
 
@@ -8,10 +8,12 @@ router.post('/',async (req,res)=>{
     try {
         const {email,amount} = req.body
     const paymentIntent = await stripe.paymentIntents.create({
+        // amount:amount*100,
         amount:amount*100,
         currency:'eur',
         metadata:{integration_check: 'accept_a_payment'},
-        receipt_email:email
+        receipt_email:email,
+        // payment_method_types: ['klarna','card'],
     })
     res.json({'client_secret': paymentIntent['client_secret']})
     console.log('Done')
